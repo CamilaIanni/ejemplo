c646e351ca9d7c65846dd1f24cf3149e12c2a306
CamilaIanni <86843950+CamilaIanni@users.noreply.github.com>
2022-04-16 09:53:30 -0300

    Create pokedex.js

---
+++ pokedex.js
@@ -0,0 +1,64 @@
+const baseURL= "https://pokeapi.co/api/v2/pokemon/"
+const appNode= document.getElementById("app")
+const pokemon= document.getElementById("pokemon")
+
+const pokeimg= document.getElementById("pokeimg")
+const pokename= document.getElementById("Pokename")
+const pokeid= document.getElementById("PokeID")
+const pokeTypes= document.getElementById("PokeTypes")
+const PokeStats=document.getElementById("PokeStats")
+
+
+
+
+
+const insertPokemon = event =>{
+    event.preventDefault();
+
+    const { value } = event.target.pokemon;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+          .then(data => data.json())   
+          .then(response => MostrarPokemon(response))
+          .catch(err => Cagamos())
+}
+
+function Cagamos(){
+    alert("Algo falló en el código....")
+}
+
+function MostrarPokemon(ObjetoPokemon){
+    
+    pokeimg.src = ObjetoPokemon.sprites.front_default;
+    pokename.textContent = `pokedex: ${ObjetoPokemon.name}`;
+    pokeid.textContent = `Numero: ${ObjetoPokemon.id}`;
+    
+    pokeTypes.innerHTML = '';
+    types = ObjetoPokemon.types;
+    types.forEach(type =>{
+        const typePokemon = document.createElement("div");
+        typePokemon.style.color = "black";
+        typePokemon.textContent = type.type.name;
+        
+        pokeTypes.appendChild(typePokemon);
+    });
+
+    PokeStats.innerHTML = '';
+    Stats = ObjetoPokemon.stats;
+
+    Stats.forEach(Stat =>{
+
+        const StatPokemon = document.createElement("div");
+        const pokeHP = document.createElement("div");
+        const pokeElement = document.createElement("div");
+
+        pokeHP.textContent= Stat.base_stat;
+        StatPokemon.textContent= Stat.stat.name;
+        
+        pokeElement.appendChild(StatPokemon);
+        pokeElement.appendChild(pokeHP);
+        PokeStats.appendChild(pokeElement);
+
+        
+    });
+
+}
